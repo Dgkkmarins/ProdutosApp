ab4b2103bd74a3848cc3f4299bbe278d77a86bd8
EdogawaCoder <edogawa.araujo@gmail.com>
2025-11-04 20:04:03 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutosApp.API/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+       [HttpGet]
+         public IActionResult GetAll()
+        {
+         return Ok("consulta realizada com sucesso");
+        
+         }
+}
+}
